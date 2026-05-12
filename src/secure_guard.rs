71468e674979ab4d6e2fec23f687b5678ec4ef75
3BK <40867233+3BK@users.noreply.github.com>
2026-05-11 22:23:56 -0400

    Update secure_guard.rs

--- src/secure_guard.rs
+++ src/secure_guard.rs
@@ -1,10 +1,11 @@
-use windows::Win32::System::Performance::*;
+use windows::Win32::System::Performance::PdhCloseQuery;
 
 pub struct PdhQueryGuard(pub isize);
 
 impl Drop for PdhQueryGuard {
     fn drop(&mut self) {
         if self.0 != 0 {
+            // Compliance: STIG requirement to release system resources
             unsafe { let _ = PdhCloseQuery(self.0); }
         }
     }
